456c25e0ff5fb96205760b59225fbb1d293b2718
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-11-06 12:11:55 -0300

    Mudando tema do bootstrap

---
+++ FIAP.Global_ImpactWeb/TagHelpers/MensageSucessTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIAP.Global_ImpactWeb.TagHelpers
+{
+    public class MensageSucessTagHelper : TagHelper
+    {
+        public string Mensagem { get; set; }
+
+        public string Classe { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if(!String.IsNullOrEmpty(Mensagem))
+            {
+                output.TagName = "div";
+                output.Attributes.SetAttribute("class", String.IsNullOrEmpty(Classe) ? "alert alert-success" : Classe);
+                output.Content.SetContent(Mensagem);
+            }
+       
+        }
+    }
+}
